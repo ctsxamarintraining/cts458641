c141061e6027980cbf4da994ae775089d1d96e3a
CTS458641 <prashant.aithal@cognizant.com>
2015-10-13 16:10:15 +0530

    eh3 added

---
+++ Day4_Afternoon/EH2/EH2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ExceptionsEx1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//			Write a program that reads an integer number and calculates and prints its square root. 
+			//			If the number is invalid or negative, print "Invalid number”. In all cases finally print "Goodbye". " +
+			//				"Use try-catch-finally.
+			try{
+				int number;
+				Console.Write ("Enter a number : ");
+				number = Convert.ToInt32 (Console.ReadLine ());
+				if(number < 0)
+					throw new NegativeNumberException("Invalid number");
+				Console.WriteLine("The square root of "+number+" is " +Math.Sqrt(number));
+			}
+			catch(FormatException e){
+				Console.WriteLine ("Invalid number");
+			}
+			catch(NegativeNumberException e){
+				Console.WriteLine (e.Message);
+			}
+			catch(Exception e){
+				Console.WriteLine ("Invalid number");
+			}
+			finally{
+				Console.WriteLine ("Goodbye");
+			}
+
+		}
+		class NegativeNumberException:ApplicationException
+		{
+			public NegativeNumberException(string message):base(message){
+				//				Console.WriteLine("Use positive numbers only");
+			}
+			// show message
+		}
+
+	}
+}
---
+++ Day4_Afternoon/EH3/EH3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionEx2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			
+			try{
+				string fpath ;
+				Console.Write("Enter file path : ");
+				fpath = Console.ReadLine();
+				Console.WriteLine("The contents of file are :\n" +File.ReadAllText(fpath));
+			}
+			catch(FormatException e){
+				Console.WriteLine ("Inside format exception");
+
+				Console.WriteLine (e.Message);
+			}
+			catch(FileNotFoundException e){
+				Console.WriteLine ("Inside file not found exception");
+
+				Console.WriteLine (e.Message);
+			}
+			catch(FileLoadException e){
+				Console.WriteLine ("Inside file load exception");
+
+				Console.WriteLine (e.Message);
+			}
+			catch(Exception e){
+				Console.WriteLine ("Inside exception");
+
+				Console.WriteLine (e.Message);
+			}
+		}
+	}
+}
