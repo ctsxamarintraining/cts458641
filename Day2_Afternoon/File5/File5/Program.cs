c87c78c5653e263d92cb4205ddeb1aa7279f2db2
CTS458641 <prashant.aithal@cognizant.com>
2015-10-13 15:12:18 +0530

    file5

---
+++ Day2_Afternoon/File5/File5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace File5
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Product product = new Product();
+
+			product.Name = "Apple";
+
+			product.Sizes = new string[] { "Small", "Medium", "Large" };
+
+			string output = JsonConvert.SerializeObject(product);
+			Console.WriteLine ("The contents of json file is \n" + output);
+			File.WriteAllText ("file.json", output);
+			//Product deserializedProduct = JsonConvert.DeserializeObject<Product>(File.ReadAllText("file.json"));
+		}
+		class Product{
+			public string Name;
+			public string[] Sizes;
+		}
+	}
+}
