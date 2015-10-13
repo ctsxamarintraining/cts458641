b64776a95cff8d6ba3e0d29352c5ba14d18e688d
CTS458641 <prashant.aithal@cognizant.com>
2015-10-13 15:58:17 +0530

    eh2 added

---
+++ Day4_Afternoon/EH1/EH1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exceptionsample
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int[] intArray = new int[10];
+			int i = 0;
+			do {
+				try{
+					Console.Write("Please add a number to array : ");
+					int ele = Convert.ToInt32(Console.ReadLine());
+					intArray[i++] = ele;
+					int j = 0;
+					string commaSepString = "";
+					for(int k=0;k<i; k++){
+						if(j==0){
+							j++;
+							commaSepString = commaSepString+intArray[k];
+						}
+						else
+							commaSepString = commaSepString + "," + intArray[k] ;
+
+
+					}
+					Console.WriteLine("The numbers in the array : "+commaSepString);
+
+				}
+				catch(IndexOutOfRangeException e){
+					Console.WriteLine(e.Message);
+				}
+				catch(FormatException e){
+					Console.WriteLine(e.Message);
+				}
+				catch(Exception e){
+					Console.WriteLine(e.Message);
+				}
+
+			} while(true);
+		}
+	}
+}
