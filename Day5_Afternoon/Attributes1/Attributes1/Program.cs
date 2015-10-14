60ce37c6f4eb7f2e4ededaedf0bf0475be894b1c
CTS458641 <prashant.aithal@cognizant.com>
2015-10-14 16:07:44 +0530

    attribute-conditional

---
+++ Day5_Afternoon/Attributes1/Attributes1/Program.cs
@@ -0,0 +1,25 @@
+﻿#define CONDITION
+#define RELEASE
+
+using System; 
+using System.Diagnostics; 
+
+class MainClass { 
+
+	[Conditional("CONDITION")]  
+	void con() { 
+		Console.WriteLine("Only if condition is defined, will this sentence appear on the screen "); 
+	} 
+
+	[Conditional("RELEASE")]  
+	void release() { 
+		Console.WriteLine("Final release version."); 
+	} 
+
+	public static void Main() { 
+		MainClass t = new MainClass(); 
+
+		t.con(); // call only if TRIAL is defined 
+		t.release(); // called only if RELEASE is defined 
+	} 
+}
