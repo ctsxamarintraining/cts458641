76d8c0bc55d393ca6fc61da9b65762becc77f7cb
CTS458641 <prashant.aithal@cognizant.com>
2015-10-16 15:17:16 +0530

    5th exercise added

---
+++ Day6/Collections5/Collections5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CollectionEx5
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//			Implement IEqualityComparer and IComparer for any custom class and give an example for both interfaces 
+			//				by using any of the default classes/methods in .NET which are accepting these interfaces.
+
+			Dictionary<Person,int> personList1 = new Dictionary<Person,int>(new MyEqualityComparer()){
+				{new Person{id=1,name="John"},1},
+				{new Person{id=2,name="Chris"},2},
+				{new Person{id=1,name="Nathan"},3}
+			};
+			Console.WriteLine ("Using IEqualityComparer");
+			foreach (Person p in personList1.Keys)
+				Console.WriteLine (p.name);
+			List<Person> myList = new List<Person> {
+				new Person{ id = 1, name = "John" },
+				new Person{ id = 6, name = "Roger" },
+				new Person{ id = 4, name = "Bryan" }
+			};
+			Console.WriteLine ("Using IComparer");
+			myList.Sort (new MyComparer ());
+			foreach(Person p in myList)
+				Console.WriteLine (p.name);
+		}
+	}
+	class Person{
+		public string name;
+		public int id;
+	}
+	class MyEqualityComparer : IEqualityComparer<Person>{
+		#region IEqualityComparer implementation
+		public bool Equals (Person x, Person y)
+		{
+			return x.id==y.id;
+		}
+		public int GetHashCode (Person obj)
+		{
+			return obj.GetHashCode ();
+		}
+		#endregion
+	}
+	class MyComparer : IComparer<Person>{
+		#region IComparer implementation
+		public int Compare (Person x, Person y)
+		{
+			return x.name.CompareTo (y.name);
+		}
+		#endregion
+	}
+}
---
+++ Day6/LinkedLIsts/LinkedLIsts/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace LinkedList
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//			Implement double LinkedList for a Person class (which contains Id, Name , Location properties) 
+			//				and use enumerator to iterate through each item and print the values on console. 
+			MyEnum myEnumObj = new MyEnum();
+			foreach (Person p in myEnumObj)
+				Console.WriteLine ("Id is {0} , Name is {1} , Location is {2}", p.Id, p.Name, p.Location);
+
+		}
+	}
+	class Person{
+		public int Id;
+		public string Name;
+		public string Location;
+	}
+	class MyEnumerator : IEnumerator{
+		public static LinkedList<Person> myPersonLinkedList;
+
+		public LinkedListNode<Person> currentObj;
+		public object Current {
+			get{
+				return currentObj.Value;
+			}
+		}
+
+		public bool MoveNext (){
+			if (myPersonLinkedList == null) {
+				myPersonLinkedList = new LinkedList<Person>();
+				myPersonLinkedList.AddLast(new Person{Id=1,Name="John",Location="hyd"});
+				myPersonLinkedList.AddLast(new Person{Id=2,Name="Daniel",Location="Delhi"});
+				myPersonLinkedList.AddLast(new Person{Id=3,Name="Popeye",Location="mumbai"});
+				myPersonLinkedList.AddLast(new Person{Id=4,Name="Tom",Location="pune"});
+			}
+			if (currentObj == null && myPersonLinkedList.First != null) {
+
+				currentObj = myPersonLinkedList.First;
+				return true;
+			} 
+			if ( currentObj.Next!=null) {
+				currentObj = currentObj.Next;
+				return true;
+			} else {
+				return false;
+			}
+
+		}
+
+		public void Reset (){
+			currentObj = null;
+		}
+	}
+	class MyEnum : IEnumerable{
+		public IEnumerator GetEnumerator (){
+			return new MyEnumerator ();
+		}
+	}
+}
