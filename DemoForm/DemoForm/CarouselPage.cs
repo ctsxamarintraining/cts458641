e2a041f3818af5b830ac80b10c8a844e14682a0b
CTS458641 <prashant.aithal@cognizant.com>
2015-11-17 17:12:42 +0530

    Demo form

---
+++ DemoForm/DemoForm/CarouselPage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinForms1
+{
+	public class CarouselPage : Xamarin.Forms.CarouselPage
+	{
+		public CarouselPage ()
+		{
+
+
+			List<ContentPage> pages = new List<ContentPage> (0);
+			Color[] colors = { Color.Red, Color.Green, Color.Blue, Color.Gray, Color.Yellow };
+			foreach (Color c in colors) {
+				pages.Add (new ContentPage { Content = new StackLayout {
+						Children = {
+							new Label { Text = "Carousel Demo" },
+							new BoxView {
+								Color = c,
+								VerticalOptions = LayoutOptions.FillAndExpand
+							}
+						}
+					}
+				});
+			}
+			this.Children.Add (pages [0]);
+			this.Children.Add (pages [1]);
+			this.Children.Add (pages [2]);
+
+
+
+
+		}
+	}
+}
